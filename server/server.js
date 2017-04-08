1c1a7e3857b160bde5e41c07e69cfa64d795eb04
sanathkodikara <sanath.kodikara@gmail.com>
2017-04-08 22:09:35 +1000

    adding the initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+//better approach than (__dirname/../public)
+const publicPath = path.join(__dirname,'../public');
+var app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`started on port ${port}....`);
+});
