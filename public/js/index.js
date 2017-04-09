787549e1408aee9ea5f1cca023e86659350f9a1a
sanathkodikara <sanath.kodikara@gmail.com>
2017-04-10 08:05:00 +1000

    emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Gen',
-    text: 'Hey hows going.I am doing well.'
-  });
 });
 
 socket.on('disconnect', function() {
